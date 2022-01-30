54b54df8e4e92638273a5fbee554e29c1530c488
muskaansinghvi <79450191+muskaansinghvi@users.noreply.github.com>
2022-01-30 17:58:51 +0530

    getting products from database

---
+++ backend/data/users.js
@@ -0,0 +1,24 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+    {
+        name: 'Muskaan Singhvi',
+        email: 'muskaan.singhvi_ug23@ashoka.edu.in',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true,
+    },
+
+    {
+        name: 'Aryan Nair',
+        email: 'aryan.nair_ug23@ashoka.edu.in',
+        password: bcrypt.hashSync('123456', 10),
+    },
+
+    {
+        name: 'Mahiya Agrawal',
+        email: 'mahiya.agrawal_ug23@ashoka.edu.in',
+        password: bcrypt.hashSync('123456', 10),
+    },
+]
+
+export default users 
